1d251b33aab10b005fb174ad8fefc20e86875642
Alan Perez <alanp9six@gmail.com>
2019-03-01 12:41:08 -0600

    classses stretch completed, math.pow was used for cube volume and surface area

--- challenges/classes.js
+++ challenges/classes.js
@@ -78,14 +78,14 @@
             length: 4,
             width: 5,
             height: 5
-    })
+    });
 // Test your volume and surfaceArea methods by uncommenting the logs below:
 console.log(cuboid.volume()); // 100
 console.log(cuboid.surfaceArea()); // 130
 
 // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
 
-class CukeMaker extends CuboidMaker
+class CubeMaker extends CuboidMaker
 {
     constructor(cubeAttr)
     {
@@ -93,11 +93,22 @@ class CukeMaker extends CuboidMaker
     }
     cubeVolume()
     {
-
+        // V=a^3
+        // Math.pow(base, exponent)
+       return  Math.pow(this.length, 3);
     }
 
     cubeSurfaceArea()
     {
-
+        // A=6a^2
+        return Math.power(this.length, 2)*6;
     }
 }
+const cube = new CubeMaker
+({
+            length: 4,
+            width: 5,
+            height: 5
+});
+    console.log(cube.cubeVolume());
+    console.log(cube.surfaceArea());
